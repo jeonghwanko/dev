faa6b6f5d4adee62a27a3e13c6da11ff56fd0561
jeonghwanko <turbo08@gmail.com>
2021-04-16 11:58:29 +0900

    apply EJS template

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 // 디폴트 라우팅, 뒤에 나오는 파라메터 title, message 는 EJS 엔진으로 넘어감.
 router.get('/', function (request, response) {
-    response.render('index', { title: 'Sample Node.js', message: 'Hello there!' });
+    response.render('index', { title: 'More Power to Big Whale', message: 'Hello there!' });
 });
 
 module.exports = router;
