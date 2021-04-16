2e59ac4432a90b68964097f715b362d1187b8c9b
jeonghwanko <turbo08@gmail.com>
2021-04-16 12:45:29 +0900

    google login

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 var express = require('express');
-// express 를 실행해서 Application 을 생성
 var app = express();
-
+var passport = require('passport')
+var session = require('express-session')
 
 // 템플릿 엔진 EJS 설정
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+// Passport setting
+app.use(passport.initialize());
+app.use(passport.session());
 
-// 정적 파일을 제공할 폴더를 지정
+// Routes
+app.use('/', require('./routes/index'));
 app.use('/public', express.static('public'));
-
-// 라우팅 설정
-app.use('/', require('./routes/index.js'));
+app.use('/auth',require('./routes/auth'));
 
 // 서버 포트 설정
-app.listen(3000);
-
-// 애플리케이션 실행 로그
-console.log('Server running at http://localhost:3000');
+var port = 3000;
+app.listen(port, function(){
+  console.log('Server running at http://localhost:3000');
+});
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('../config/passport.js')
 
 // 디폴트 라우팅, 뒤에 나오는 파라메터 title, message 는 EJS 엔진으로 넘어감.
 router.get('/', function (request, response) {
-    response.render('index', { title: 'More Power to Big Whale!', message: 'Hello there!' });
+    response.render('index', {
+      title: 'More Power to Big Whale!',
+      user: request.user,
+      message: 'Hello there!'
+    });
 });
 
 module.exports = router;
